a04614f9dc93ff6d4fb1dd5f97fd13fed3044a30
Robert McGregor <me@robertmcgregor.ca>
2014-04-09 17:51:30 -0400

    adding process.php and writing web form

--- index.php
+++ index.php
@@ -61,7 +61,21 @@
 	    	</div>
 
 			<div>
-				<form action="process.php" method="POST" id="braintree-payment-form">
+				<form action="customer.php" method="POST" id="braintree-payment-form">
+					<h2>Customer Information</h2>
+					<p>
+						<label>First Name</label>
+						<input type="text" name="first_name" id="first_name"></input>
+					</p>
+					<p>
+						<label for="last_name">Last Name</label>
+						<input type="text" name="last_name" id="last_name"></input>
+					</p>
+					<p>
+						<label for="postal_code">Postal Code</label>
+						<input type="text" name="postal_code" id="postal_code"></input>
+					</p>
+					<h2>Credit Card</h2>
 					<p>
 						<label>Card Number</label>
 						<input type="text" size="20" autocomplete="off" data-encrypted-name="number" />
@@ -72,10 +86,15 @@
 					</p>
 					<p>
 						<label>Expiration (MM/YYYY)</label>
-						<input type="text" size="2" name="month" /> / <input type="text" size="4" name="year" />
+						<input type="text" size="2" data-encrypted-name="month" /> / <input type="text" size="4" data-encrypted-name="year" />
 					</p>
-					<input type="submit" id="submit" />
+					<input class="submit-button" type="submit" />
 				</form>
+				<script src="https://js.braintreegateway.com/v1/braintree.js"></script>
+				<script>
+				var braintree = Braintree.create("YourClientSideEncryptionKey");
+				braintree.onSubmitEncryptForm("braintree-payment-form");
+				</script>
 			</div>
 
 
