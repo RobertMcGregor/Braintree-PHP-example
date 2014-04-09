be3c3efdbfa858b16d84591547dcaee187e6bea9
Robert McGregor <me@robertmcgregor.ca>
2014-04-09 18:49:35 -0400

    adding process.php and writing web form

--- customer.php
+++ customer.php
@@ -8,7 +8,13 @@
     echo "<pre>";
     print_r($_POST);
     echo "</pre> <br /><br />";
-
+    // require braintree
+    require_once 'lib/braintree-php-2.27.0/lib/Braintree.php';
+    // configure braintree
+    Braintree_Configuration::environment('sandbox');
+    Braintree_Configuration::merchantId('444s7mg9tmz62x24');
+    Braintree_Configuration::publicKey('sn37xjfq972bbpvz');
+    Braintree_Configuration::privateKey('c9230a35c0f109d5095fb9448c2dfcd3');
 
     $result = Braintree_Customer::create(array(
         "firstName" => $_POST["first_name"],
