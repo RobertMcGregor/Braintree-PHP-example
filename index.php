472178ee4ebab40a4ee7e1a52e88486a237a2ac1
Robert McGregor <me@robertmcgregor.ca>
2014-04-09 15:37:05 -0400

    adding process.php and writing web form

--- index.php
+++ index.php
@@ -51,6 +51,10 @@
 
 		<div class="container">
 
+			<br />
+			<br />
+
+
 	    	<div class="starter-template">
 	    		<h1>Bootstrap starter template</h1>
 	    		<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
