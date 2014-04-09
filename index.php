70faa5d37ac1b60d509f950e8d415b8c5fb5ee0e
Robert McGregor <robert@qu-dmws-3986.local>
2014-04-09 15:21:46 -0400

    adding process.php and writing web form

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-<?php
-
-	require_once 'lib/braintree-php-2.27.0/lib/Braintree.php';
-
-	Braintree_Configuration::environment('sandbox');
-	Braintree_Configuration::merchantId('444s7mg9tmz62x24');
-	Braintree_Configuration::publicKey('sn37xjfq972bbpvz');
-	Braintree_Configuration::privateKey('c9230a35c0f109d5095fb9448c2dfcd3');
-
-?>
-
 <html>
 
 	<head>
@@ -20,64 +9,37 @@
 
 		<title>Welcome to TVOD</title>
 
-		<!-- socialize.js script should only be included once -->
-		<script type="text/javascript" src="http://cdn.gigya.com/js/socialize.js?apiKey=3_6Xi2j7Oip7qk2BhjndSy6sxfG1mL-M5VABBF-vc3NZJ0CdssXTapV-z-Zlqh1rbM">
-		{
-			siteName: 'dev.robertmcgregor.ca'
-			,enabledProviders: 'facebook,twitter,linkedin,paypal'
-		}
-		</script>
-
 	</head>
 
 	<body>
 
-
-
-		<br />
-
-		<script type="text/javascript">
-		var login_params=
-		{
-			showTermsLink: 'false'
-			,height: 230
-			,width: 310
-			,UIConfig: '<config><body><controls><snbuttons buttonsize="50" /></controls></body></config>'
-			,autoDetectUserProviders: ''
-			,facepilePosition: 'none'
-		}
+		<h1>TVOD</h1>
+		<div>
+			<form action="transaction.php" method="POST" id="braintree-payment-form">
+				<p>
+					<label>Card Number</label>
+					<input type="text" size="20" autocomplete="off" data-encrypted-name="number" />
+				</p>
+				<p>
+					<label>CVV</label>
+					<input type="text" size="4" autocomplete="off" data-encrypted-name="cvv" />
+				</p>
+				<p>
+					<label>Expiration (MM/YYYY)</label>
+					<input type="text" size="2" name="month" /> / <input type="text" size="4" name="year" />
+				</p>
+				<input type="submit" id="submit" />
+			</form>
+		</div>
+	
+		<script src="https://js.braintreegateway.com/v1/braintree.js"></script>
+		<script>
+		var braintree = Braintree.create("YourClientSideEncryptionKey");
+		braintree.onSubmitEncryptForm('braintree-payment-form');
 		</script>
-		<div id="componentDiv"></div>
-		<script type="text/javascript">
-		   gigya.socialize.showLoginUI(login_params);
-		</script>
-
-		<?= process(); ?>
 
 	</body>
 
 </html>
-
-<?php
-	
-	function process($amount='1000.00', $number='5105105105105100', $expirationMonth='05', $expirationYear='12' ){
-		$result = Braintree_Transaction::sale(array(
-		    'amount' => $amount,
-		    'creditCard' => array(
-		        'number' => $number,
-		        'expirationMonth' => $expirationMonth,
-		        'expirationYear' => $expirationYear
-		    )
-		));
-
-		if ($result->success) {
-		    return "success!: " . $result->transaction->id;
-		} else if ($result->transaction) {
-			return "Error processing transaction: \n  message: " . $result->message . "\n  code: " . $result->transaction->processorResponseCode . "\n  text: " . $result->transaction->processorResponseText;
-		} else {
-		    return "Message: " . $result->message . "\nValidation errors: \n" . $result->errors->deepAll();
-		}
-	}
-?>
 
 
---
+++ process.php
@@ -0,0 +1,43 @@
+<?php 
+/*********************************************************
+*
+* Process.php
+* where all form information comes to get processed 
+* TODO add subscription type
+*
+/*********************************************************/
+
+	// require braintree
+	require_once 'lib/braintree-php-2.27.0/lib/Braintree.php';
+	// configure braintree
+	Braintree_Configuration::environment('sandbox');
+	Braintree_Configuration::merchantId('444s7mg9tmz62x24');
+	Braintree_Configuration::publicKey('sn37xjfq972bbpvz');
+	Braintree_Configuration::privateKey('c9230a35c0f109d5095fb9448c2dfcd3');
+
+
+
+	if($_POST){
+		echo process($_POST['amount'],$_POST['number'],$_POST['expirationMonth'], $POST['expirationYear']); 
+	}
+	
+	public function process($amount='1000.00', $number='5105105105105100', $expirationMonth='05', $expirationYear='12' ){
+		$result = Braintree_Transaction::sale(array(
+		    'amount' => $amount,
+		    'creditCard' => array(
+		        'number' => $number,
+		        'expirationMonth' => $expirationMonth,
+		        'expirationYear' => $expirationYear
+		    )
+		));
+
+		if ($result->success) {
+		    return "success!: " . $result->transaction->id;
+		} else if ($result->transaction) {
+			return "Error processing transaction: \n  message: " . $result->message . "\n  code: " . $result->transaction->processorResponseCode . "\n  text: " . $result->transaction->processorResponseText;
+		} else {
+		    return "Message: " . $result->message . "\nValidation errors: \n" . $result->errors->deepAll();
+		}
+	}
+
+?>
