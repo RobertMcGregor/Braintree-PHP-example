38ba97782e5569b706e47b5bfc5513dc781622d3
Robert McGregor <me@robertmcgregor.ca>
2014-04-10 09:56:45 -0400

    adding ajax submit

--- index.php
+++ index.php
@@ -110,12 +110,6 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		
-		<!-- Braintree JS include --> 
-		<script src="https://js.braintreegateway.com/v1/braintree.js"></script>
-		<script>
-		var braintree = Braintree.create("MIIBCgKCAQEA4r8HobL/ayOzyAiSOTelqXZ1+0MfH0JTj3mjeYnPm3eqv88jVcv1Bvs2A/Rr1/wlKPCWyL33ZvoMgqMM3ETYWZdb0t+UnUi+/c8cHyjp1YT7//L0gDgGtzwLkypQ+r7vhXG2B09YYxW0gTn6nvtse0POwNyz5+IGu7u3Xfr2rVePNHm22zmxwlf2/kzEaWdz6OTNFjN+5RRo1A1oefE0AgQeMLUigLZyN69k8Ex/thbOrO3HzWuA4xC2o0lnrayE/YQavzX8m9v1ddr3Lp6airzERK2LD9Atr5Ech0fXDENT5Q8KXgT3XDgntSSVmyVGZ");
-		braintree.onSubmitEncryptForm('braintree-payment-form');
-		</script>
 
 
 		<script>
