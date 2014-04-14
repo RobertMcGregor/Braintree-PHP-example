e7d527cbb6655de448c10e089d092ab707557b0e
Robert McGregor <me@robertmcgregor.ca>
2014-04-14 08:58:06 -0400

    demo 1 done1

--- index.php
+++ index.php
@@ -11,7 +11,14 @@ require_once 'menu.php';
 
 	    	<div class="starter-template">
 	    		<h1>Braintree, Gigya and TVOD</h1>
-	    		<p class="lead">This is a quick test to get braintree, gigya talking for the TVOD project.</p>
+	    		<p class="lead">
+	    			This is a quick test to get braintree, gigya talking for the TVOD project.
+	    		</p>
+
+	    		<p>
+	    			<h2> Demo 1 </h2>
+	    			<a href="./demo.php">Use SSO account inforamtion to auto populate subscription form.</a>
+	    		</p>
 	    	</div>
 
 	    	<!-- show demo pages! --> 
